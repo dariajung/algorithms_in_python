c938fce450fa58be29fb082b4109d50825eb4bc2
Daria Jung <djj2115cu@gmail.com>
2013-12-19 17:30:35 -0600

    refactor so it memoizes chain lengths

--- problem_14.py
+++ problem_14.py
@@ -1,25 +1,33 @@
 # Which starting number, under one million, produces the longest Collatz chain?
 
+memoized = {}
+
 def Collatz(n):
-
-    chain = [n]
-
-    while n != 1:
-        if n % 2 == 0:
-            n /= 2
-        else:
-            n = 3*n + 1
-        chain.append(n)
-
-    return chain
+    global memoized
+    if n in memoized:
+        return memoized[n]
+    else:
+        start = n
+        chain = 1
+        while n != 1:
+            if n in memoized:
+                memoized[start] = memoized[n] + chain
+                return memoized[n] + chain
+            if n % 2 == 0:
+                n /= 2
+            else:
+                n = 3*n + 1
+            chain += 1
+        memoized[start] = chain
+        return chain
 
 def longest_chain():
     number = 0
     chain_len = 0
-    for i in range(1000001, 0, -1):
+    for i in range(1, 1000001):
         chain = Collatz(i)
-        if len(chain) > chain_len:
-            chain_len = len(chain)
+        if chain > chain_len:
+            chain_len = chain
             number = i
 
     return number, chain_len
